1dfa7c90097355ff981954a7e9723fafadd8e473
= <=>
2023-10-28 18:19:00 -0300

    Variáveis

---
+++ variaveis.py
@@ -0,0 +1,29 @@
+x = 5 # x recebe o valor 5
+y = 3.5
+z = 1+2j
+
+w = "Ana"
+w = 'Ana'
+
+#A-z, 0-9 e _
+#altura, Altura e ALTURA
+
+h, j, l = 1, 3, 5
+print(h)
+print(j)
+print(l)
+
+a = b = c = " faz o curso de Python3"
+
+print(a)
+print(b)
+print(c)
+
+h = w + a
+print(h)
+
+l = x + j
+print(l)
+
+l = w + x
+print(l)
