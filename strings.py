42d28a2191050510bd5dc8b85cc4b94ff7722e49
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2024-05-19 13:16:44 -0300

    Strings

---
+++ strings.py
@@ -0,0 +1,32 @@
+print("olá")
+print('ola')
+
+w = "Curso de Python3"
+print(w)
+
+"""comentário legal - string que não está guardada na memória """
+
+a = """olá, 
+este é um texto
+em bloco no 
+pyton"""
+
+print(a)
+
+# Métodos de Strings
+b = " Ola Mundo "
+# ignora os espaços no começo e no final da string
+print(b.strip())
+
+c = 'Gustavo Alves da Silva'
+# tudo em letras minúsculas
+print(c.lower())
+
+d = 'curso de python'
+print(d.upper())
+
+# concatenar strings
+e = "Ola"
+f = "Mundo"
+g = e + " " + f
+print(g)
