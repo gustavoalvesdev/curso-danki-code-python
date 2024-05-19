06e7daa2c2228b93482571159fc0f9b2bf952f0d
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2024-05-19 13:30:18 -0300

    Casting

---
+++ casting.py
@@ -0,0 +1,37 @@
+# casting para conversão de dados
+
+x = 2 # int
+y = 2.8 # float
+z = '2' # string
+
+x = int(x)
+y = int(y)
+z = int(z)
+
+print(x, y, z)
+
+#float
+
+a = 2.3
+b = 2
+c = '2.3'
+d = '2'
+
+a = float(a)
+b = float(b)
+c = float(c)
+d = float(d)
+
+print(a, b, c, d)
+
+t = 's1'
+r = 2
+z = 2.3
+
+t = str(t)
+r = str(r)
+z = str(z)
+
+print(' A variável t é do tipo: ', type(t))
+print(' A variável r é do tipo: ', type(r))
+print(' A variável z é do tipo: ', type(z))
